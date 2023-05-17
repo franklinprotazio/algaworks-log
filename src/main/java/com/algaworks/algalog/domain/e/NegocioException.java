78e308a7b661b36e00c9e46d1c7bcfb0fac03270
franklinprotazio <protaziolancamentos@gmail.com>
2023-05-17 20:01:52 -0300

    Segundo dia aula

---
+++ src/main/java/com/algaworks/algalog/domain/e/NegocioException.java
@@ -0,0 +1,12 @@
+package com.algaworks.algalog.domain.e;
+
+public class NegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public NegocioException(String message) {
+		super(message);
+	}
+
+	
+}
